a069c275cab63f604acc27f89523b735bf7bc26d
Kathleen Tonner <kathleentonner@gmail.com>
2020-04-07 19:43:38 -0400

    client-side post

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 var path = require("path");
+const fs = require("fs")
 
 const PORT = process.env.PORT || 8080
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -27,25 +28,28 @@ app.get("/notes", function (req, res) {
 
 //GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 
-app.get("/api/notes", function(req,res){
+app.get("/api/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"))
 })
 
 //  * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, 
 //and then return the new note to the client.
 
-app.post("/api/notes", function(req,res){
+app.post("/api/notes", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     var newNote = req.body
     console.log(newNote)
-    notes.push(newNote)
     res.json(newNote)
+    fs.readFile('/db/db.json', function (err, data) {
+        var json = JSON.parse(data)
+        json.push(newNote)
+        fs.writeFile("/db/db.json", JSON.stringify(json))
 
-})
+    })
 
 
-//Server listening
+    //Server listening
 
-app.listen(PORT, () =>{
-    console.log(`Listening on Port ${PORT}`)
-})
+    app.listen(PORT, () => {
+        console.log(`Listening on Port ${PORT}`)
+    })
